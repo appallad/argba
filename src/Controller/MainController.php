aff53f5f5ef76c59f855561bc67e31a799bf9f57
appallad <appallad@gmail.com>
2018-05-24 18:29:25 +0300

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,3 +9,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Doctrine\DBAL\Driver\Connection;
+
+class MainController extends Controller
+{
+
+    /**
+     * @Route("/")
+     */
+
+    public function number(Connection $conn)
+    {
+        $number = mt_rand(0, 144);
+
+        $nodes = $conn->fetchAll('SELECT * FROM node_field_data LIMIT 10');
+        $aliases = $conn->fetchAll('SELECT * FROM url_alias LIMIT 10');
+
+        return $this->render('main/index.html.twig', array(
+            'number' => $number,
+            'nodes' => $nodes,
+            'aliases' => $aliases,
+        ));
+
+    }
+}
