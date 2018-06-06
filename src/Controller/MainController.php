ff997a8c9b8ddb0e4ce70168ba1ee0bf7a82da02
appallad <appallad@gmail.com>
2018-06-06 11:32:17 +0300

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -30,7 +30,32 @@ class MainController extends Controller
 
         $homeId = str_replace('/node/', "", $homeNode['source']); // id of the main profile in CMS DB
         
-        $homeImageset = $conn->fetchAll('SELECT DISTINCT node__field_imageset.field_imageset_target_id AS image_id, node__field_imageset.delta AS delta, node__field_imageset.field_imageset_title AS image_title, node__field_imageset.field_imageset_alt AS image_alt, file_managed.filename as filename, file_managed.uri as uri  FROM node__field_imageset LEFT JOIN file_managed ON node__field_imageset.field_imageset_target_id = file_managed.fid WHERE node__field_imageset.entity_id = "'.$homeId.'" AND node__field_imageset.langcode = "uk" ORDER BY delta ASC');
+        $homeImageset = $conn->fetchAll('SELECT DISTINCT
+         node__field_imageset.field_imageset_target_id AS image_id,
+         node__field_imageset.delta AS delta,
+         node__field_imageset.field_imageset_title AS image_title,
+         node__field_imageset.field_imageset_alt AS image_alt,
+         file_managed.filename AS filename,
+         file_managed.uri AS uri
+         FROM node__field_imageset
+         LEFT JOIN file_managed
+         ON node__field_imageset.field_imageset_target_id = file_managed.fid
+         WHERE node__field_imageset.entity_id = "'.$homeId.'"
+         AND node__field_imageset.langcode = "uk"
+         ORDER BY delta ASC');
+
+        $profileImage = $conn->fetchAssoc('SELECT
+         node__field_image.field_image_target_id AS image_id,
+         node__field_image.field_image_title AS image_title,
+         node__field_image.field_image_alt AS image_alt,
+         file_managed.filename AS filename,
+         file_managed.uri AS uri
+         FROM node__field_image
+         LEFT JOIN file_managed
+         ON node__field_image.field_image_target_id = file_managed.fid
+         WHERE node__field_image.entity_id = "'.$homeId.'"
+         AND node__field_image.langcode = "uk"');
+
 
         return $this->render('main/index.html.twig', array(
             'number' => $number,
@@ -38,6 +63,7 @@ class MainController extends Controller
             'aliases' => $aliases,
             'home_id' => $homeId,
             'home_imageset' => $homeImageset,
+            'profile_image' => $profileImage,
         ));
 
     }
